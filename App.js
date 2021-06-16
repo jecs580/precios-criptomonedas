651c6ffaa5a607cc97c2a30a7dfda60f3228b929
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-06-15 23:25:31 -0400

    Obteniendo los datos desde CoinGecko

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useEffect, useState} from 'react'
+import { View, Text, FlatList } from 'react-native'
 
-export default function App() {
+const App = () => {
+  const [coins, setcoins] = useState([])
+  const loadData=async ()=>{
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+    const data = await res.json();
+    setcoins(data)
+  }
+  useEffect(()=>{
+    loadData();
+  },[])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <FlatList
+        data={coins}
+        renderItem={({item})=>{
+          console.log(item);
+          return <Text>{item.name}</Text>
+        }}
+      />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
