53f591a440e08e72ae0d2c1e9fcd989389733fa4
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-06-18 21:48:46 -0400

    Agregando funcionalidad al buscador de criptomonedas

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { View, Text, FlatList, StyleSheet, TextInput, StatusBar } from 'react-na
 import CoinItem from './components/CoinItem'
 const App = () => {
   const [coins, setcoins] = useState([])
+  const [search, setSearch] = useState('')
   const loadData=async ()=>{
     const res = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
     const data = await res.json();
@@ -19,11 +20,19 @@ const App = () => {
       />
       <View style={styles.header}>
         <Text style={styles.title}>CryptoMarket</Text>
-        <TextInput style={styles.searchInput}/>
+        <TextInput style={styles.searchInput}
+          placeholder="Search a Coin"
+          placeholderTextColor="#858585"
+          onChangeText={text=>setSearch(text)}
+        />
       </View>
       <FlatList 
         style={styles.list}
-        data={coins}
+        data={
+          coins.filter(coin=>
+            coin.name.toLowerCase().includes(search)|| coin.symbol.toLowerCase().includes(search)
+          )
+        }
         renderItem={({item})=>{
           return <CoinItem coin={item}/>
         }}
